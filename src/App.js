8be0926858c857e725d1399bd7cca95c9771b6f4
jmerlosrepo <77901200+jmerlosrepo@users.noreply.github.com>
2021-01-29 21:05:22 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,29 @@ import React, {Component} from 'react';
 import Navigation from './components/nav';
 import Filter from './components/filter';
 import JewelsList from './components/jewelsList';
+import ImageModal from './components/imageModal';
 import './App.css';
+import jewelImg from './assets/jewels/jewel.jpg';
 
 class App extends Component {
+
+  state = {
+    modalJewelImage: jewelImg,
+    modalJewelVisible: false,
+    jewels: [{id: 1, category: 'ring', image: 'jewel.jpg', description: 'A blue jewel that is used as an example for this little demo and see if everything works correctly.', price: 25000}] 
+  }
+
+  handleHideShowModal = () =>{
+    this.setState({modalJewelVisible: !this.state.modalJewelVisible});
+  }
+
   render(){
     return (
       <div>
         <Navigation />
         <Filter />
-        <JewelsList />
+        <JewelsList jewels={this.state.jewels} onShowHideModal={this.handleHideShowModal} />
+        <ImageModal onCloseModal={this.handleHideShowModal} visible={this.state.modalJewelVisible} image={this.state.modalJewelImage} />
       </div>
       );
   }
