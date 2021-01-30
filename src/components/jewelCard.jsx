62453cfd9a8335a33a69b8ea5b695fc3f67f4ae7
jmerlosrepo <77901200+jmerlosrepo@users.noreply.github.com>
2021-01-29 21:04:13 -0600

    Update jewelCard.jsx

--- src/components/jewelCard.jsx
+++ src/components/jewelCard.jsx
@@ -1,16 +1,29 @@
 import React, {Component} from 'react';
-import jewelImg from '../assets/jewels/jewel.jpg'; 
 
 class JewelCard extends Component {
     render(){
+        const {jewel, onShowHideModal} = this.props;
+        const {image, id, category, description, price} = jewel;
+
         return(
             <div className="jewelCard">
                 <div className="cardImage">
-                    <img src={jewelImg} alt="jewel"/>
+                    <img onClick={onShowHideModal} src={`http://localhost:3000/jewels/${image}`} alt="jewel"/>
                 </div>
                 <div className="jewelDetails">
-                   <div className="label">Id: </div> 
-                   <div className="label">Descripcion: </div>
+                   <div>
+                    <div className="label">Clave: </div>
+                    <div className="description">{id}</div>
+                   </div>
+                   <div>
+                    <div className="label">Categoria: </div>
+                    <div className="description">{category}</div> 
+                   </div>
+                   <div>
+                    <div className="label">Descripcion: </div>
+                    <div className="description">{description}</div>
+                   </div>
+                   {!price || <div><div className="label">Precio: </div><div className="description">${price}</div></div>}
                 </div>
             </div>
         )
