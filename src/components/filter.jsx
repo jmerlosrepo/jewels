5d92a29425b70620f900a0947cd666d68db45a4c
jmerlosrepo <77901200+jmerlosrepo@users.noreply.github.com>
2021-01-31 21:04:27 -0500

    Update filter.jsx

--- src/components/filter.jsx
+++ src/components/filter.jsx
@@ -1,17 +1,24 @@
-import React,{Component} from 'react';
+import React, { Component } from "react";
 
-class Filter extends Component{
-    render(){
-        return(
-        <div className="filterContainer">
-          <form>
-            <label htmlFor="">Buscar: </label>
-            <input type="text" placeholder="Escribe lo que quieras buscar..."/>
-            <button>Buscar</button>
-          </form>
-        </div>
-        );
-    }
+class Filter extends Component {
+  render() {
+    const { onSearch } = this.props;
+    return (
+      <div className="filterContainer">
+        <form>
+          <label htmlFor="">Buscar: </label>
+          <input type="text" placeholder="Escribe lo que quieras buscar..." />
+          <button
+            onClick={(e) => {
+              onSearch(e, "ring");
+            }}
+          >
+            Buscar
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Filter;
