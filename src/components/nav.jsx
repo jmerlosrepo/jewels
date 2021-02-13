743127f70d8ae254456cf6703564ab1e4a5c1220
jmerlosrepo <77901200+jmerlosrepo@users.noreply.github.com>
2021-02-12 23:40:21 -0600

    Update nav.jsx

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import logo from '../assets/logo.png';
+import ShoppingCartCounter from './shoppingCartCounter';
 
-const Navigation = (props) =>{
+const Navigation = ({jewelsCount}) =>{
     return(
         <div className="navContainer">
-          <img src={logo} alt="logo"/>
           <nav>
             <ul>
               <li><a href="">Inicio</a></li>
               <li><a href="">Contacto</a></li>
             </ul>
           </nav>
+          <div className="social">
+            <a target="_blank" href="#">
+            <i className="fa fa-facebook-official" aria-hidden="true"></i>
+            </a>
+            <a target="_blank" href="#">
+            <i className="fa fa-instagram" aria-hidden="true"></i>
+            </a>
+          </div>
+          <ShoppingCartCounter jewelsCount={jewelsCount}/>
         </div>
     );
 }
