024b1159dd8f947dbff589ba45c1ecd0260f05a4
jmerlosrepo <77901200+jmerlosrepo@users.noreply.github.com>
2021-02-18 22:44:24 -0600

    Update contactForm.jsx

--- src/components/contactForm.jsx
+++ src/components/contactForm.jsx
@@ -2,9 +2,26 @@ import React, { Component } from "react";
 import SelectedJewelCard from "./selectedJewelCard";
 
 class ContactForm extends Component {
-  render() {
-    const { jewelsToQuery, onRemoveJewelFromCart } = this.props;
 
+  state = {
+    order: {
+      name: "",
+      email: "",
+      phone: "",
+      message: "",
+      products: []
+    }
+  }
+
+  handleFieldChanges = ({currentTarget: input}) => {
+    const order = {...this.state.order}
+      order[input.name] = input.value;  
+    this.setState({order});
+  }
+
+  render() {
+    const { jewelsToQuery, onRemoveJewelFromCart} = this.props;
+    const {name, email, phone, message, products} = this.state.order;
     return (
       <div className="contactForm grayBakgroundColor">
         <fieldset>
@@ -12,24 +29,25 @@ class ContactForm extends Component {
           <form>
             <div className="controlGroup">
               <label htmlFor="">Nombre: </label>
-              <input type="text" />
+              <input onChange={this.handleFieldChanges} name="name" type="text" value={name} />
             </div>
             <div className="controlGroup">
               <label htmlFor="">Correo: </label>
-              <input type="email" />
+              <input onChange={this.handleFieldChanges} name="email" type="email" value={email} />
             </div>
             <div className="controlGroup">
               <label htmlFor="">Telefono: </label>
-              <input type="phone" />
+              <input onChange={this.handleFieldChanges} name="phone" type="phone" value={phone} />
             </div>
             <div className="controlGroup">
               <label htmlFor="">Mensaje: </label>
               <textarea
+                onChange={this.handleFieldChanges}
                 className="messageLabel"
-                name=""
-                id=""
+                name="message"
                 cols="30"
                 rows="10"
+                value={message}
               ></textarea>
             </div>
             <div className="cartProducts">
